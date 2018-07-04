2df314b92bb01eb1c522e48f800c559679de49ec
krishnanath <isabella0484@gmail.com>
2018-07-04 08:04:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,39 +52,6 @@ app.post('/signin', (req, res) => {
 
 app.post('/register',(req , res) => { register.handleRegister(req , res, db, bcrypt)})
 
-
-app.post('/register', (req,res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-db.transaction(trx => {
-    trx.insert({
-        hash: hash,
-        email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-    
-        return trx('users')
-.returning('*')
-.insert({
-    email: loginEmail[0],
-    name: name,
-    joined: new Date()
-}).then(user => {
-    res.json(user[0]);
-})
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-})
-   
- 
-.catch(err => res.status(400).json('already exits')) 
-})
-
- 
-
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     db.select('*').from('users').where({id})
